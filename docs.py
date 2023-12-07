54d69bae1f55caf183c6eb44c78ba07371329895
Minh Luan <donguyenminhluan96@gmail.com>
2023-12-07 10:29:20 +0700

    Create docs.py

---
+++ docs.py
@@ -0,0 +1,33 @@
+'''
+  https://drive.google.com/file/d/1bba9rvxCe7ovQRT20JcomkHxs6DpEkf-/view
+
+  https://drive.google.com/file/d/18iTzgO0l8tKjebnnNW60L9G5BMifum-L/view
+
+  https://drive.google.com/file/d/1AwTcYOVmv6lETYNYA0xgN1BQRvcY4X4M/view
+
+  https://drive.google.com/file/d/1XcaeORPhu33J3-YITv8JOVYFsNTVq6Tw/view
+
+  https://drive.google.com/file/d/1KfkNVKrhkWQSjHBWV_NQC04pt5A6GImk/view
+
+  https://drive.google.com/file/d/1z5Gl5GNFnI6vwCqnNSg_wcYXGLQs6T-q/view
+
+  https://drive.google.com/file/d/10LnaS9l8ZAzVVYjSaYTDQhhKC8rMzYdm/view
+
+  https://drive.google.com/file/d/1RaTGMdi19lPUh7aFRCamaG_XPKoCSM-O/view
+
+  https://drive.google.com/file/d/12Eb5xpPLIemTemO11uIRIFNqfNdGxbAj/view
+
+  https://drive.google.com/file/d/1F0kXGS1-ND4OcOsLKiYcxnM4j9hsQZHn/view
+
+  https://drive.google.com/file/d/1VC3RDyP0w7F9p5br-4p3K-yjHusV_nNh/view
+
+  https://drive.google.com/file/d/16UtHg0TDTghmN3OFKQOfidrQqNNSdNdZ/view
+
+  https://drive.google.com/file/d/1GBWePA6F_epAMqq9V2gvHAAA1pt8oGTW/view
+
+  https://drive.google.com/file/d/1uYpJGg9wv1xrLuJDORzASQJE-0nPI6WW/view
+
+  https://drive.google.com/file/d/1VKF9NFIbCMGjeCwbNNOOGPdTbdi6Gxxu/view
+
+  
+'''
